b1601fec81374343a7e5d1e5a9dcf49351d38dce
tactnician <tactnician@gmail.com>
2019-02-08 10:37:43 -0500

    more styling changes

--- src/components/footer/arrow.js
+++ src/components/footer/arrow.js
@@ -1,12 +1,24 @@
 
 class ArrowNav {
-    constructor(arrow){
-        this.arrow = arrow;
-
-        this.arrowData = this.arrow.dataset.tab; //stores a string with the id of next card
-        console.log(this.arrowData);
+    constructor(arrows){
+        this.arrowRight = arrows.right;
+        this.arrowLeft = arrows.left;
     }
 }
 
-let arrows = document.querySelectorAll('.arrow').forEach(arrow => new ArrowNav(arrow));
-console.log
+ (function($) {
+        $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
+            {
+            // height: 6000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
+            onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
+                console.log(percent);
+            }
+        });
+    }(jQuery));
+
+
+
+let Arrows = {
+    left: document.querySelector('.arrow-left'),
+    right: document.querySelector('.arrow-right')
+}
