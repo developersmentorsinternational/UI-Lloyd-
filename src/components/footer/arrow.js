f4dcf0e89bc9cdedd296256c3353e3a2dc39ce9f
tactnician <tactnician@gmail.com>
2019-02-08 14:18:03 -0500

    READ.md updated

--- src/components/footer/arrow.js
+++ src/components/footer/arrow.js
@@ -11,7 +11,11 @@ class ArrowNav {
             {
             // height: 6000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
             onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
-                console.log(percent);
+                //import ArrowNav from arrow.js
+                
+                // percent > 33 ? document.setAttirbute ( href of arrow )
+                // percent > 66 ?
+                // percent > 33 ?
             }
         });
     }(jQuery));
